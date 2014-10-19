15d5685b8145da221acb2dc1431c884be2b296ce
Nick Mathew <nim8@georgetown.edu>
2014-10-19 09:44:55 +0300

    Configuration for HTTPS sessions

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -458,3 +458,8 @@ ini_set('session.cookie_lifetime', 2000000);
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
+
+/**
+ * Allow mixed-mode HTTP and HTTPS sessions
+ */
+//$conf['https'] = TRUE;
